d937329e25f68dae2343a90f184303327b0b7691
theroggy <pieter.roggemans@gmail.com>
2026-04-03 17:59:11 +0200

    try 26

--- segmodels_keras/_compat.py
+++ segmodels_keras/_compat.py
@@ -1,11 +1,4 @@
 import keras
 from packaging.version import parse as parse_version
 
-try:
-    keras_version = parse_version(keras.__version__)
-except Exception:
-    import tf.keras as tf_keras
-
-    keras_version = parse_version(tf_keras.__version__)
-
-KERAS_GTE_3 = keras_version >= parse_version("3.0.0")
+KERAS_GTE_3 = parse_version(keras.__version__) >= parse_version("3.0.0")
